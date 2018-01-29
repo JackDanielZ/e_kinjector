b1310bdf2802d40f831da7d7c1be12272e4eaf57
Daniel Zaoui <daniel.zaoui@yahoo.com>
2018-01-29 10:46:24 +0200

    Fix install paths by using pkg-config

--- make.sh
+++ make.sh
@@ -5,20 +5,21 @@ gcc -shared -fPIC -DPIC src/e_mod_main.o `pkg-config --libs enlightenment elemen
 [ $? -eq 0 ] || exit 1
 
 #Edje
-/opt/e/bin/edje_cc -v -id ./images e-module-kinjector.edc e-module-kinjector.edj
+edje_cc -v -id ./images e-module-kinjector.edc e-module-kinjector.edj
 [ $? -eq 0 ] || exit 1
-/opt/e/bin/edje_cc -v -id ./images kinjector.edc kinjector.edj
+edje_cc -v -id ./images kinjector.edc kinjector.edj
 [ $? -eq 0 ] || exit 1
 
 # Test app
 gcc -g src/e_mod_main.c -DSTAND_ALONE $CFLAGS `pkg-config --cflags --libs elementary` -o src/e_kinjector
 [ $? -eq 0 ] || exit 1
 
+prefix=$(pkg-config --variable=prefix enlightenment)
 release=$(pkg-config --variable=release enlightenment)
 host_cpu=$(uname -m)
 MODULE_ARCH="linux-gnu-$host_cpu-$release"
 
 sudo /usr/bin/mkdir -p '/opt/e/lib/enlightenment/modules/kinjector/'$MODULE_ARCH
-sudo /usr/bin/install -c src/module.so /opt/e/lib/enlightenment/modules/kinjector/$MODULE_ARCH/module.so
-sudo /usr/bin/install -c module.desktop /opt/e/lib/enlightenment/modules/kinjector/module.desktop
-sudo /usr/bin/install -c -m 644 e-module-kinjector.edj kinjector.edj '/opt/e/lib/enlightenment/modules/kinjector'
+sudo /usr/bin/install -c src/module.so $prefix/lib/enlightenment/modules/kinjector/$MODULE_ARCH/module.so
+sudo /usr/bin/install -c module.desktop $prefix/lib/enlightenment/modules/kinjector/module.desktop
+sudo /usr/bin/install -c -m 644 e-module-kinjector.edj kinjector.edj $prefix/lib/enlightenment/modules/kinjector
